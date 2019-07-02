42d13bb0cae84e8c4f61a453ae31780e0206deee
Pannio <1821299140@qq.com>
2019-07-02 22:23:53 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,2 +1,7 @@
 <?php
 
+// 将路由转化为每个页面的css类名
+function route_class()
+{
+    return str_replace('.','-', Route::currentRouteName());
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    // 避免浏览器缓存
+    .version();
