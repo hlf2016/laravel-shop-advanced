01d24e78e904997b84f9d7cb94ee4e4f04094385
Pannio <1821299140@qq.com>
2019-07-04 08:21:45 +0000

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Auth::routes(['verify' => true]);
 // auth中间件代表需要登录 verified代表需要邮箱验证
 Route::group(['middleware'=>['auth', 'verified']], function(){
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    // 新建地址
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    // 存储地址
+    Route::post('user_addresses/store', 'UserAddressesController@store')->name('user_addresses.store');
 });
