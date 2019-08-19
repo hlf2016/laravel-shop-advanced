211651a3168e738ba71a85038df211c1395fdcd8
Pannio <1821299140@qq.com>
2019-08-19 12:48:28 +0000

    管理后台sql文件‘

---
+++ database/admin.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 5.7.25, for osx10.14 (x86_64)
+--
+-- Host: 192.168.25.228    Database: shop-test
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `admin_menu`
+--
+
+LOCK TABLES `admin_menu` WRITE;
+/*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2019-07-08 13:22:53'),(2,0,2,'系统管理','fa-tasks',NULL,NULL,NULL,'2019-07-08 13:23:12'),(3,2,3,'管理员','fa-users','auth/users',NULL,NULL,'2019-07-08 13:23:26'),(4,2,4,'角色','fa-user','auth/roles',NULL,NULL,'2019-07-08 13:23:55'),(5,2,5,'权限','fa-ban','auth/permissions',NULL,NULL,'2019-07-08 13:24:07'),(6,2,6,'菜单','fa-bars','auth/menu',NULL,NULL,'2019-07-08 13:24:16'),(7,2,7,'操作日志','fa-history','auth/logs',NULL,NULL,'2019-07-08 13:24:27'),(8,0,0,'用户管理','fa-users','/users',NULL,'2019-07-12 07:03:43','2019-07-12 07:03:43'),(9,0,0,'商品列表','fa-bar-chart','/products',NULL,'2019-07-14 09:26:28','2019-07-14 09:26:28'),(10,0,0,'订单管理','fa-reorder','orders',NULL,'2019-08-14 17:18:57','2019-08-14 17:18:57'),(11,0,0,'优惠券','fa-barcode','/coupon_codes',NULL,'2019-08-19 11:15:59','2019-08-19 11:15:59');
+/*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_permissions`
+--
+
+LOCK TABLES `admin_permissions` WRITE;
+/*!40000 ALTER TABLE `admin_permissions` DISABLE KEYS */;
+INSERT INTO `admin_permissions` VALUES (1,'All permission','*','','*',NULL,NULL),(2,'Dashboard','dashboard','GET','/',NULL,NULL),(3,'Login','auth.login','','/auth/login\r\n/auth/logout',NULL,NULL),(4,'User setting','auth.setting','GET,PUT','/auth/setting',NULL,NULL),(5,'Auth management','auth.management','','/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs',NULL,NULL),(6,'用户管理','users','','/users*','2019-07-14 07:38:40','2019-07-14 07:38:40');
+/*!40000 ALTER TABLE `admin_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_role_menu`
+--
+
+LOCK TABLES `admin_role_menu` WRITE;
+/*!40000 ALTER TABLE `admin_role_menu` DISABLE KEYS */;
+INSERT INTO `admin_role_menu` VALUES (1,2,NULL,NULL);
+/*!40000 ALTER TABLE `admin_role_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_role_permissions`
+--
+
+LOCK TABLES `admin_role_permissions` WRITE;
+/*!40000 ALTER TABLE `admin_role_permissions` DISABLE KEYS */;
+INSERT INTO `admin_role_permissions` VALUES (1,1,NULL,NULL),(2,3,NULL,NULL),(2,4,NULL,NULL),(2,6,NULL,NULL),(2,2,NULL,NULL);
+/*!40000 ALTER TABLE `admin_role_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_role_users`
+--
+
+LOCK TABLES `admin_role_users` WRITE;
+/*!40000 ALTER TABLE `admin_role_users` DISABLE KEYS */;
+INSERT INTO `admin_role_users` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL);
+/*!40000 ALTER TABLE `admin_role_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_roles`
+--
+
+LOCK TABLES `admin_roles` WRITE;
+/*!40000 ALTER TABLE `admin_roles` DISABLE KEYS */;
+INSERT INTO `admin_roles` VALUES (1,'Administrator','administrator','2019-07-08 13:05:08','2019-07-08 13:05:08'),(2,'运营','operator','2019-07-14 07:40:20','2019-07-14 07:40:20');
+/*!40000 ALTER TABLE `admin_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_user_permissions`
+--
+
+LOCK TABLES `admin_user_permissions` WRITE;
+/*!40000 ALTER TABLE `admin_user_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin_user_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_users`
+--
+
+LOCK TABLES `admin_users` WRITE;
+/*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
+INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$u9kU82R8jl8W2df1jXFeTOzAZjykVxvBZWXBsy7/PVIzdDoIlAl7G','Administrator',NULL,NULL,'2019-07-08 13:05:08','2019-07-08 13:05:08'),(2,'operator','$2y$10$Fqih7WWeMMuVgTFMaWU8u.VnLJ.iBadPeN1qZUcm0gj4rLKm9XGmG','运营','images/山.jpg',NULL,'2019-07-14 07:42:11','2019-07-14 07:42:11');
+/*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-08-19 20:38:44
