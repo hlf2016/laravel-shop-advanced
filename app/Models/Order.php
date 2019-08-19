0802c886f86c0f505b4367dfb6515fd178175733
Pannio <1821299140@qq.com>
2019-08-19 06:13:04 +0000

    管理后台 - 优惠券列表

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\CouponCode;
 use Ramsey\Uuid\Uuid;
 use Illuminate\Database\Eloquent\Model;
 
@@ -111,5 +112,10 @@ class Order extends Model
         } while (self::query()->where('refund_no', $no)->exists());
 
         return $no;
+    }
+
+    public function couponCode()
+    {
+        return $this->belongsTo(CouponCode::class);
     }
 }
